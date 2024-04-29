b96f0fe23daeb5417df3823f63e2f26e5f7dafda
Karolina Pater <karolinapater1995@gmail.com>
2024-04-29 23:22:16 +0200

    build

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://karolinapater.github.io/to-do-list-vue/',
   define: { 'process.env': {} },
   plugins: [vue(), vueJsx(), svgLoader()],
   resolve: {
